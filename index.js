85d4bbc36299a9dfef1c057a7d7e1ff870014685
abdumurod9911 <khasanboyevabdumurod2@gmail.com>
2022-12-19 19:02:04 +0300

    fix

---
+++ index.js
@@ -0,0 +1,33 @@
+// 1.Argument sifatida ikkita raqamni (a va b) oladigan funksiya yozing. a va b yig'indisi. Natijani qaytaring
+
+// function MyFunc(a, b){
+//     return a + b
+// }
+// console.log(MyFunc(2, 5))
+
+
+
+// 2. Argument sifatida ikkita qiymatni, masalan, a va b qiymatlarini qabul qiladigan funksiya yozing. Ikki qiymat teng va bir xil turdagi bo'lsa, true qiymatini qaytaring
+
+// function
+// myFunction(a, b){
+//     return  a===b ? true:false 
+// }
+// console.log(myFunction("3", "3"));
+
+
+// 3. Argument sifatida satr (a) va sonni (n) oladigan funksiya yozing. "a" ning n-belgisini qaytaring.
+
+// function myFunction (a, n){
+//     return a[n-1]
+// }
+// console.log(myFunction('jack' , 4));
+
+
+
+//  4.Argument sifatida (a) qatorni oladigan funksiya yozing. a ning dastlabki 3 ta belgisini olib tashlang. Natijani qaytaring
+
+// function myFunction(a){
+//          return a.slice(3)
+//      }
+//      console.log(myFunction("absdefg"));
